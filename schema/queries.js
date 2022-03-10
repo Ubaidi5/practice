ce6b06f36f18ecb455b59644c0ebbc2c5f7dca3d
ubaidi5 <ubaid7625@gmail.com>
2022-03-10 23:45:42 +0500

    removed unused queries

--- schema/queries.js
+++ schema/queries.js
@@ -12,53 +12,16 @@ const { GraphQLObjectType, GraphQLList, GraphQLString, GraphQLFloat, GraphQLID, 
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
-    sendEmail: {
-      type: Types.USER_TYPE,
-      args: {
-        firstName: { type: GraphQLString },
-        lastName: { type: GraphQLString },
-        email: { type: GraphQLString },
-        phoneNumber: { type: GraphQLString },
-      },
-      resolve: async (_, args) => {
-        const templateData = {
-          firstName: args.firstName,
-          lastName: args.lastName,
-          email: args.email,
-          phoneNumber: args.phoneNumber,
-          password: "args.password",
-        };
-
-        emailHelper.emailDocument(
-          "ubaid.uok@gmail.com",
-          "xpgyzq7c8",
-          "ubaid@yopmail.com",
-          "Request for reset password",
-          "views/emailTemplates/signup_without_password_template.html",
-          "Request for reset password",
-          templateData,
-          function (isError, data) {
-            // console.log("Error Occured", isError);
-            // console.log("Response of email", data);
-          }
-        );
-        const user = await userModel.findOne({ email: args.email });
-        if (user) {
-          return user;
-        } else {
-          throw new Error("User not found");
-        }
-      },
-    },
     getAllMembers: {
       type: new GraphQLList(Types.MEMBER_TYPE),
       args: {},
-      resolve: async () => {
+      resolve: async (parent, args, request) => {
         try {
+          userController.verifyJWT(request.headers);
           const allMembers = await userController.getAllMembers();
           return allMembers;
         } catch (err) {
-          return err;
+          throw err;
         }
       },
     },
